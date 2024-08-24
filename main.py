c5edeb4e8053e0f740af30bfe8cbf01a51300e39
ryaan <ryaanamank91@gmail.com>
2024-08-25 01:47:32 +0200

    Day 21: Add second row as description

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import streamlit as st
+from PIL import Image
 
 st.set_page_config(layout="wide")
 
 # Display the homepage header
 st.header("Home", anchor=None)
 
-# create two columns in the webpage
+# create two columns for row1 in the webpage
+column_left_row1, column_right_row1 = st.columns(2)
 
-column_left, column_right = st.columns(2)
+# First column and row1 content
+with column_left_row1:
+    # image = st.image('images/photo.jpg')
+    # Load image
+    img = Image.open('images/photo.jpg')
 
-# First column content
-with column_left:
-    image = st.image('images/photo.jpg')
+    # Resize the image
+    img_resized = img.resize((500, 600))  # width: 300px and height: 200px
 
-# Second Column content
-with column_right:
+    # Display the resized image
+    st.image(img_resized)
+
+# Second Column and row1 content
+with column_right_row1:
     st.title("Ryaan Amank Semwal")
-    content = """
+    content1 = """
     Hi, I am Ryaan Amank Semwal! I am a Technical Project Lead @EXCELTIC, a global leader in providing consulting 
     solutions in the automotive sector, with my current focus on sensor fusion and upcoming microcontrollers 
     for automotive.. 
@@ -24,4 +32,10 @@ with column_right:
     I have successfully led and managed teams and projects in product development, system engineering, and 
     requirement analysis for ADAS and infotainment systems. """
 
-    description = st.info(content)
+    description = st.info(content1)
+
+# Add second row
+content2 = """
+Below you can find some of the apps I have built in Python. Feel free to contact me!
+"""
+brief = st.write(content2)
